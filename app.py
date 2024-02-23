be8049e67846aa42b5425556def6d5c19d7590de
wavey0x <retrowaveyy@gmail.com>
2024-02-23 09:05:14 -0500

    add timestamp endpoint

--- app.py
+++ app.py
@@ -179,6 +179,43 @@ def get_contract_function_output(web3, address, abi, function_name, args=[]):
     function = getattr(contract.functions, function_name)
     return function(*args).call()
 
+@app.route('/tools/timestamp', methods=['GET'])
+@app.route('/tools/ts', methods=['GET'])
+def convert_timestamp():
+    response = {'is_valid': False, 'message': ''}
+    unix_timestamp = request.args.get('ts') or request.args.get('timestamp')
+    try:
+        unix_timestamp = int(unix_timestamp)
+    except ValueError as e:
+        app.logger.error(f"{e}")
+        return jsonify({"error": 'Invalid input. Please pass a timestamp as integer.'}), 400
+    
+    import datetime
+    import pytz
+    from dateutil.relativedelta import relativedelta
+
+    # Convert Unix timestamp to UTC
+    utc_time = datetime.datetime.utcfromtimestamp(unix_timestamp).replace(tzinfo=pytz.utc)
+
+    # Calculate relative time
+    now = datetime.datetime.now(pytz.utc)
+    if now > utc_time:
+        delta = relativedelta(now, utc_time)
+        relative_time = f"{abs(delta.years)} years, {abs(delta.months)} months, {abs(delta.days)} days, " \
+                        f"{abs(delta.hours)} hours, {abs(delta.minutes)} minutes ago"
+    else:
+        delta = relativedelta(utc_time, now)
+        relative_time = f"in {delta.years} years, {delta.months} months, {delta.days} days, " \
+                        f"{delta.hours} hours, {delta.minutes} minutes"
+    
+    response = {
+        'unix_timestamp': unix_timestamp,
+        'utc_time': utc_time,
+        # 'utc_time_string': utc_time.strftime('%Y-%m-%d %H:%M:%S %Z'),
+        'relative_time': relative_time
+    }
+    return response
+
 @app.route('/curve/verify_gauge', methods=['GET'])
 def gauge_check():
     response = {'is_valid': False, 'message': ''}
