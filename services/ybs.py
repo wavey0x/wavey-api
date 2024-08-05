496fa79c2895dbc2bbbbfb2a1be7794f852a475b
wavey0x <retrowaveyy@gmail.com>
2024-08-05 19:34:01 -0400

    feat: filter on token not ybs

--- services/ybs.py
+++ services/ybs.py
@@ -3,13 +3,13 @@ import pandas as pd
 from models import UserWeekInfo, CrvLlHarvest
 import json
 
-def user_info():
+def user_info(request):
     account = request.args.get('account', 1, type=str)
     week_id = request.args.get('week_id', 1, type=int)
-    ybs = request.args.get('ybs', 1, type=int)
+    token = request.args.get('token', 1, type=str)
     print(account)
     print(week_id)
-    results = UserWeekInfo.query.filter_by(account=account, week_id=week_id, ybs=ybs).all()
+    results = UserWeekInfo.query.filter_by(account=account, week_id=week_id, token=token).all()
     
     if not results:
         return jsonify([])
